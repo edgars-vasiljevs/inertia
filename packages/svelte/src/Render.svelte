ff500caf60ed0fa723eb1ca652ce65e4dee19d95
Edgars Vasiljevs <edgars@simpleweb.lv>
2023-09-08 21:19:05 +0300

    fix svelte page component render on same page visit

--- packages/svelte/src/Render.svelte
+++ packages/svelte/src/Render.svelte
@@ -16,10 +16,10 @@
   export let children = []
 </script>
 
-{#if $store.component}
+{#key $store.key}
   <svelte:component this={component} {...props}>
     {#each children as child, index (component && component.length === index ? $store.key : null)}
       <svelte:self {...child} />
     {/each}
   </svelte:component>
-{/if}
+{/key}
